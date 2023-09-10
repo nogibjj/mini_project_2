1d03a6dcba592bb9ebcd6497ca6526968c787fd2
Bobby Zhang <z.zhang19@imperial.ac.uk>
2023-09-10 17:54:08 -0400

    add new

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 [![CI](https://github.com/nogibjj/python-template/actions/workflows/cicd.yml/badge.svg)](https://github.com/nogibjj/python-template/actions/workflows/cicd.yml)
-## mini project 2: desriptive statistical template 
+# mini project 2: desriptive statistical template 
+
+## Overview 
+in this project 
 
 
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Main cli or app entry point
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # define a function to provide descriptive statistics of a dataset
 
@@ -10,5 +11,21 @@ import pandas as pd
 def desripStats(file):
     # use panda to read csv file
     df = pd.read_csv(file)
+
+    # quick glimpse of the data
+    print(df.head())
+
+    # check data information
+    print(df.info())
+
+    # check unique values for each colunm
+    print(df.nunique())
+
+    # plot a scatter graph
+    plt.scatter(df["wt"], df["mpg"])
+    plt.xlabel("Weight, lbs")
+    plt.ylabel("Miles per Gallon, miles")
+    plt.title("Miles per gallon changes with automible weight")
+    plt.show()
 
     return df.describe()
