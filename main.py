ae072f23b8b4ebc93ede3eaf5f1348164e00f393
Bobby Zhang <z.zhang19@imperial.ac.uk>
2024-09-10 11:37:34 -0400

    updates

--- main.py
+++ main.py
@@ -8,21 +8,18 @@ import matplotlib.pyplot as plt
 # define a function to provide descriptive statistics of a dataset
 
 
-def desripStats(file):
-    # use panda to read csv file
-    df = pd.read_csv(file)
+def desripStats(dataframe):
+    return dataframe.describe()
 
-    # check data information
-    print(df.info())
 
+def plotScatter(df):
     # plot a scatter graph
     plt.scatter(df["wt"], df["mpg"])
     plt.xlabel("Weight, lbs")
     plt.ylabel("Miles per Gallon, miles")
     plt.title("Miles per gallon changes with automible weight")
     plt.show()
-
-    return  # df.describe()
+    return
 
 
 def findMin(data):
@@ -50,8 +47,10 @@ def calcMean(data):
 
 if __name__ == "__main__":
     file = "https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv"
-    desripStats(file)
     data = pd.read_csv(file)
+
+    # descriptive statistics
+    desripStats(data)
 
     # run some test cases: mean, min and max of the 'mpg'
     # minimum
@@ -65,3 +64,5 @@ if __name__ == "__main__":
     # mean
     mean_mpg = calcMean(data["mpg"])
     print("mean of 'mpg': ", mean_mpg)
+
+    print(desripStats(data))
--- test_main.py
+++ test_main.py
@@ -3,42 +3,63 @@ Test goes here
 
 """
 
-from main import desripStats, findMin, findMax, calcMean
+from main import desripStats, findMin  # , findMax, calcMean
 import pandas as pd
+import requests
 
 
-def test_pd_descriptive():
-    # dataset file
-    file = "https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv"
+def check_url_exists(url):
+    try:
+        # Send a GET request to the URL
+        response = requests.head(url, allow_redirects=True)
 
-    print("this code reads csv data from: ", file)
+        # Check if the status code is in the range of success codes (200-299)
+        if 200 <= response.status_code < 300:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as e:
+        # Handle exceptions (e.g., network problems, invalid URL, etc.)
+        print(f"Error occurred: {e}")
+        return False
 
-    data_summary = desripStats(file)
 
-    # data = pd.read_csv(file)["mpg"]
+# run some test cases: mean, min and max of the 'mpg'
+# minimum
+def test_findMin(file):
+    data = pd.read_csv(file)["mpg"]
+    min_mpg = findMin(data)
+    return min_mpg
 
-    # run some test cases: mean, min and max of the 'mpg'
-    # minimum
-    # try:
-    #     min_mpg = findMin(data)
-    #     assert data_summary["mpg"]["min"] == min_mpg
-    # except AssertionError as msg:
-    #     raise msg + "minimum result unmatch"
 
-    # # maximum
-    # try:
-    #     max_mpg = findMax(data)
-    #     assert data_summary["mpg"]["max"] == max_mpg
-    # except AssertionError as msg:
-    #     raise msg + "maximum result unmatch"
+# try:
+#     min_mpg = findMin(data)
+#     assert data_summary["mpg"]["min"] == min_mpg
+# except AssertionError as msg:
+#     raise msg + "minimum result unmatch"
 
-    # # mean
-    # try:
-    #     mean_mpg = calcMean(data)
-    #     assert round(data_summary["mpg"]["mean"], 3) == mean_mpg
-    # except AssertionError as msg:
-    #     raise msg + "mean result unmatch"
+# # maximum
+# try:
+#     max_mpg = findMax(data)
+#     assert data_summary["mpg"]["max"] == max_mpg
+# except AssertionError as msg:
+#     raise msg + "maximum result unmatch"
+
+# # mean
+# try:
+#     mean_mpg = calcMean(data)
+#     assert round(data_summary["mpg"]["mean"], 3) == mean_mpg
+# except AssertionError as msg:
+#     raise msg + "mean result unmatch"
 
 
 if __name__ == "__main__":
-    test_pd_descriptive()
+    url = "https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv"
+    assert check_url_exists(url)
+    dataframe = pd.read_csv(url)
+    # test findMin
+    assert test_findMin(url) == desripStats(dataframe)["mpg"]["min"]
+    # test findMax
+    # assert findMax(data) == 33.9
+    # # test calcMean
+    # assert calcMean(data) == 20.091
