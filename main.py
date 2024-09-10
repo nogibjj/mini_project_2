8ba13a0520cd759c2a283b56913ce4481475e2a5
Bobby Zhang <z.zhang19@imperial.ac.uk>
2024-09-10 10:52:49 -0400

    update main and test

--- main.py
+++ main.py
@@ -12,14 +12,8 @@ def desripStats(file):
     # use panda to read csv file
     df = pd.read_csv(file)
 
-    # quick glimpse of the data
-    print(df.head())
-
     # check data information
     print(df.info())
-
-    # check unique values for each colunm
-    print(df.nunique())
 
     # plot a scatter graph
     plt.scatter(df["wt"], df["mpg"])
@@ -28,4 +22,46 @@ def desripStats(file):
     plt.title("Miles per gallon changes with automible weight")
     plt.show()
 
-    return df.describe()
+    return  # df.describe()
+
+
+def findMin(data):
+    min_ind = 0
+    for i in range(len(data)):
+        if data[i] < data[min_ind]:
+            min_ind = i
+    return data[min_ind]
+
+
+def findMax(data):
+    max_ind = 0
+    for i in range(len(data)):
+        if data[i] > data[max_ind]:
+            max_ind = i
+    return data[max_ind]
+
+
+def calcMean(data):
+    total = 0
+    for ele in data:
+        total += ele
+    return round(total / len(data), 3)
+
+
+if __name__ == "__main__":
+    file = "https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv"
+    desripStats(file)
+    data = pd.read_csv(file)
+
+    # run some test cases: mean, min and max of the 'mpg'
+    # minimum
+    min_mpg = findMin(data["mpg"])
+    print("minimum of 'mpg': ", min_mpg)
+
+    # maximum
+    max_mpg = findMax(data["mpg"])
+    print("maximum of 'mpg': ", max_mpg)
+
+    # mean
+    mean_mpg = calcMean(data["mpg"])
+    print("mean of 'mpg': ", mean_mpg)
